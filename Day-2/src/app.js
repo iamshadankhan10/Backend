ba1f26cc1ce6832d45498a63425d9fb73f527dcd
Shadan Raza Khan <iamshadankhan10@gmail.com>
2026-03-17 03:22:20 +0530

    Added Day 1 to Day 4 backend practice folders

---
+++ Day-2/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+const notes = []
+
+app.post("/notes", (req,res)=>{
+    console.log(req.body)
+})
+
+module.exports = app
---
+++ Day-3-Database/server.js
@@ -0,0 +1,8 @@
+// serber ko start karna hai
+
+const app = require('./src/app')
+
+app.listen(3000, ()=>{
+    console.log("Server is running");
+    
+})
