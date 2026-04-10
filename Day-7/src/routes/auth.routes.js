45cd8a23d4e1b8833197e4383c205d50a2929f7a
Shadan Raza Khan <iamshadankhan10@gmail.com>
2026-04-11 03:17:36 +0530

    Added Day-7 project

---
+++ Day-7/src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+//register api is file main create hogi and then app.js file main isko use karenge
+const express = require("express")
+
+const authRoutes = express.Router()
+
+
+module.exports = authRouter
