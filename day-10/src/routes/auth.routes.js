478f1032a1c082970037fcfb18068d9fc95a3488
Shadan Raza Khan <iamshadankhan10@gmail.com>
2026-04-20 16:05:29 +0530

    insta clone initiate

--- day-10/src/routes/auth.routes.js
+++ day-10/src/routes/auth.routes.js
@@ -1,112 +1,20 @@
 const express = require("express");
-const userModel = require("../models/user.model");
-const crypto = require("crypto");
 const authRouter = express.Router();
-const jwt = require("jsonwebtoken");
+const authController = require("../controllers/auth.controller");
 
 /*
     Route: /api/auth/register
     Method: POST
 */
-authRouter.post("/register", async (req,res)=>{
-    const {email, username, password, bio, profileImage} = req.body;
+authRouter.post("/register", authController.registerController);
 
-    // const isUserExistByEmail = await userModel.findOne({email});
+/*
+    Route: /api/auth/login
+    Method: POST
+*/
+authRouter.post("/login", authController.loginController);
 
-    // if(isUserExistByEmail){
-    //     return res.status(409).json({message: "User with this email already exists"});
-    // }
-
-    // const isUserExistsByUsername = await userModel.findOne({username});
-
-    // if(isUserExistsByUsername){
-    //     return res.status(409).json({message: "User with this username already exists"});
-    // }
-
-
-    // Yaha pe bss ek baaar hi humne database ko query kiya hai instead of 2 baar, isse performance improve hoti hai
-    const isUserAlreadyExists = await userModel.findOne({
-        $or: [
-            {email},
-            {username}
-    ]});
-
-    if(isUserAlreadyExists){
-        return res.status(409)
-        .json({
-            message: "User already exists" + (isUserAlreadyExists.email==
-                email ? "Email already exists" : "Username already exists")
-        });
-    }
-
-    const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-    const user = await userModel.create({
-        username,
-        email,
-        bio,
-        profileImage,
-        password: hash
-    });
-
-    const token = jwt.sign({
-        id: user._id
-    }, process.env.JWT_SECRET, {expiresIn: "1d"});
-
-    res.cookie("token", token);
-
-    res.status(201).json({
-        message: "User registered successfully",
-        user: {
-            username: user.username,
-            email: user.email,
-            bio: user.bio,
-            profileImage: user.profileImage
-        }
-    });
-})
-
-
-authRouter.post("/login", async (req,res)=>{
-    const {username, email, password} = req.body;
-
-    const user = await userModel.findOne({
-        $or: [
-            {username: username},
-            {email: email}
-        ]
-    });
-
-    if(!user){
-        return res.status(404).json({message: "User not found"});
-    }
-
-    const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-    const ispasswordValid = hash === user.password;
-
-    if(!ispasswordValid){
-        return res.status(401).json({
-            message: "Invalid credentials"
-        });
-    }
-
-    const token = jwt.sign({
-        id: user._id
-    }, process.env.JWT_SECRET, {expiresIn: "1d"});
-
-    res.cookie("token", token);
-
-    res.status(200).json({
-        message: "User logged in successfully",
-        user: {
-            username: user.username,
-            email: user.email,
-            bio: user.bio,
-            profileImage: user.profileImage
-        }
-    });
-});
+module.exports = authRouter;
 
 
 
