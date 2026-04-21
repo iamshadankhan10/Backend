8005528e63540a826f6e2cfea6ea95040fdf5d85
Shadan Raza Khan <iamshadankhan10@gmail.com>
2026-04-22 03:43:08 +0530

    Post creation and verification done

--- day-10/src/routes/post.routes.js
+++ day-10/src/routes/post.routes.js
@@ -11,4 +11,18 @@ const upload = multer({ storage: multer.memoryStorage() });
 */
 postRouter.post("/", upload.single("image"), postController.createPostController);
 
+/**
+ * GET /api/posts/ [protected]
+ */
+
+postRouter.get("/", postController.getPostController);
+
+/**
+ * GET /api/posts/details/:postId
+ * - return a detail about specific post with the id, also check whether the post belongs to the user that is request come from.
+ */
+
+postRouter.get("/details/:postId", postController.getPostDetailsController);
+
+
 module.exports = postRouter;
