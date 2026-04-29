9cd5548156e7f310236a84eff7004789ba4ebeaf
Shadan Raza Khan <iamshadankhan10@gmail.com>
2026-04-29 15:48:28 +0530

    Add follow model with follower and following references

--- day-10/src/models/user.model.js
+++ day-10/src/models/user.model.js
@@ -20,7 +20,23 @@ const userSchema = new mongoose.Schema({
     profileImage: {
         type: String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-    }
+    },
+
+    /**
+     * followers and following are
+     * arrays of ObjectIds that reference
+     * the users collection. This allows us to easily retrieve the followers and following 
+     * of a user by populating these fields when querying the database.
+     * 1 follower -> 12 bytes
+     */
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    }]
 })
 
 const userModel = mongoose.model("users", userSchema)
